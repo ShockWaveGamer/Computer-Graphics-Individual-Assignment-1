2041f9b67d89e8711c0fe55bec88a3d22d9111f5
ShockWaveGamer <daniel.fiuk21@gmail.com>
2023-02-05 16:23:17 -0500

    Added LUT

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -37,24 +37,8 @@ public class GameManager : MonoBehaviour
 
         if (objectives.Count <= 0)
         {
-            StartCoroutine(Win());
-        }
-
-        IEnumerator Win()
-        {
-            Debug.Log("Win");
-            
-            while (Time.timeScale > 0)
-            {
-                Time.timeScale = Mathf.Clamp(Time.timeScale - Time.unscaledDeltaTime, 0, Mathf.Infinity);
-                yield return new WaitForEndOfFrame();
-            }
-
             Instantiate(winScreen, transform.Find("Canvas"));
             GameObject.Find("CM vcam1").SetActive(false);
-
-            yield return new WaitForSeconds(1f);
-            Application.Quit();
         }
     }
 }
